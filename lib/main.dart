4ffdb5a6b985d54636b4599bae3e3a0ef3530e0a
phatG004 <gibpattarawan@gmail.com>
2025-09-26 19:30:29 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
